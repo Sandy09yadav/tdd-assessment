2c1ddb0d898abaa98b5e1259540836eb099ce56b
sandy09yadav <sonu09yadav03@example.com>
2025-07-17 18:46:35 +0530

    feat: return number if one number is provided

--- test/stringCalculator.test.js
+++ test/stringCalculator.test.js
@@ -4,4 +4,8 @@ describe('String Calculator', () => {
     test('returns 0 for empty string', () => {
         expect(add("")).toBe(0);
     });
+
+    test('returns number for single input', () => {
+        expect(add("1")).toBe(1);
+    });
 });
